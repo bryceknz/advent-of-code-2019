9eb351b643db6117ea10cc88c487d1a7d1938bd5
Bryce Kehoe <bryce@kehoe.co.nz>
2019-12-14 21:41:49 +1300

    Simplified test cases

--- day-03/classes/Line.test.js
+++ day-03/classes/Line.test.js
@@ -3,7 +3,7 @@ const Point = require('./Point')
 
 describe('Line class tests', () => {
   const pointA = new Point(0, 0)
-  const pointB = new Point(10, 10)
+  const pointB = new Point(4, 0)
   const line = new Line(pointA, pointB)
 
   it('constructor returns an instance of Line with correct values', () => {
@@ -13,11 +13,11 @@ describe('Line class tests', () => {
   })
 
   it('intersect returns correct point of intersection', () => {
-    const pointC = new Point(10, 0)
-    const pointD = new Point(0, 10)
+    const pointC = new Point(2, -2)
+    const pointD = new Point(2, 2)
     const otherLine = new Line(pointC, pointD)
 
-    const expected = new Point(5, 5)
+    const expected = new Point(2, 0)
 
     const actual = line.intersect(otherLine)
     expect(actual).toEqual(expected)
